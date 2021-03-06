6616784ef8a1a926a74d06790c609e23640cf200
KieranSingh2 <singhkieran298@gmail.com>
2021-03-06 15:58:35 -0500

    Created function in translate.py to return all supported languages.

--- translate.py
+++ translate.py
@@ -1,13 +1,17 @@
 from google.cloud import translate
 from config import *
 import os
+import googletrans
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= KEY_PATH
+# TODO: find way to convert between full language name to google language code
 
-def translate_text(text="YOUR_TEXT_TO_TRANSLATE", src_lang= "en-US", targ_lang= "ja"):
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = KEY_PATH
+
+
+def translate_text(text="YOUR_TEXT_TO_TRANSLATE", src_lang="en-US", targ_lang="ja"):
     """Translating Text."""
-    project_id= PROJECT_ID
-    
+    project_id = PROJECT_ID
+
     client = translate.TranslationServiceClient()
 
     location = "global"
@@ -28,12 +32,13 @@ def translate_text(text="YOUR_TEXT_TO_TRANSLATE", src_lang= "en-US", targ_lang= 
 
     # Display the translation for each input text provided
     # for translation in response.translations:
-        # print("Translated text: {}".format(translation.translated_text))
+    # print("Translated text: {}".format(translation.translated_text))
     return response.translations[0].translated_text
+
 
 def detect_language(text):
     """Detecting the language of a text string."""
-    project_id=PROJECT_ID
+    project_id = PROJECT_ID
 
     client = translate.TranslationServiceClient()
 
@@ -56,7 +61,7 @@ def detect_language(text):
     #     print("Language code: {}".format(language.language_code))
     #     # Confidence of detection result for this language
     #     print("Confidence: {}".format(language.confidence))
-    
+
     return response.languages[0].language_code
 
 
@@ -64,6 +69,9 @@ def get_translated_text(text, targ_lang):
     return translate_text(text, detect_language(text), targ_lang)
 
 
+def get_supported_languages():
+    """Getting a list of supported language codes."""
 
+    all_languages = {v: k for k, v in googletrans.LANGUAGES.items()}
 
-
+    return all_languages
